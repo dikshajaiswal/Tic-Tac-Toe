aad144ea60788852e6f058e4d18930ab496151f3
Diksha Jaiswal <dikshajaiswal@Dikshas-MacBook-Pro.local>
2024-07-31 19:16:19 -0400

    React: Tic-Tac-Toe: Create Player Names and Symbols
    Lecture 74 & 75
    in order to avoid repeating the same code for each players multiple times
    we make a separate components for the same and also add a button
    which will be used for editing player name
    
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Player from './components/Player.jsx'
 
 function App() {
   
 
   return (
-    <p>Coming Soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol = "X"/>
+          <Player name="Player 2" symbol = "O"/>
+        </ol>
+        Game Board
+      </div>
+      </main>
+  );
 }
 
 export default App
