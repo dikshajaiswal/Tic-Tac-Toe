3afdd1c640bc27a11fa1727339b3b0458110a56d
Diksha Jaiswal <dikshajaiswal111@gmail.com>
2024-08-05 23:08:34 -0400

    React: Tic-Tac-Toe: make edit button work
    Lecture 78
    User Input and two-way binding
    
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [ playerName, setPlayerName ] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick(){
       setIsEditing((editing) => !editing);
       console.log(isEditing);
     }
+    function handleChange(event){
+      setPlayerName(event.target.value);
+    }
     // let btnCaption = "Edit";
-    let playerName = <span className="player-name">{name}</span>;
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
 
     if(isEditing){
-      playerName = <input type="text" required value={name}/>;
+      editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
       // btnCaption="Save";
     }
-
 
     return (
             <li>
               <span className="player">
-                {playerName}
+                {editablePlayerName}
                 {/* <span className="player-name">{name}</span> */}
                 <span className="Player Symbol">{symbol}</span>
               </span>
