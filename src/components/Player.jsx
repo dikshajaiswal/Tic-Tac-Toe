fa56d747488f1d9186062d84633144fae7cdda6b
Diksha Jaiswal <dikshajaiswal111@gmail.com>
2024-08-05 22:13:11 -0400

    React: Tic-Tac-Toe: update state using function when updating based on old states
    Till lectire 78
    
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 
 export default function Player({name, symbol}) {
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    function handleEditClick(){
+      setIsEditing((editing) => !editing);
+      console.log(isEditing);
+    }
+    // let btnCaption = "Edit";
+    let playerName = <span className="player-name">{name}</span>;
+
+    if(isEditing){
+      playerName = <input type="text" required value={name}/>;
+      // btnCaption="Save";
+    }
+
 
     return (
             <li>
               <span className="player">
-                <span className="player-name">{name}</span>
+                {playerName}
+                {/* <span className="player-name">{name}</span> */}
                 <span className="Player Symbol">{symbol}</span>
               </span>
-              <button>Edit</button>
+              {/* <button>Edit</button> */}
+              {/* <button onClick={handleEditClick}>{btnCaption}</button> */}
+              <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
             </li>
     );
   }
