0b3cc2dd24bd437b433be68661276711a4c0c3f8
Diksha Jaiswal <dikshajaiswal111@gmail.com>
2024-08-08 15:07:39 -0400

    React: Tic-Tac-Toe: create game board
    Lecture 80
    
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import Player from './components/Player.jsx'
+import GameBoard from './components/GameBoard.jsx';
 
 function App() {
-  
+
 
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player initialName="Player 1" symbol = "X"/>
-          <Player initialName="Player 2" symbol = "O"/>
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
-        Game Board
+        <GameBoard />
       </div>
-      </main>
+    </main>
   );
 }
 
