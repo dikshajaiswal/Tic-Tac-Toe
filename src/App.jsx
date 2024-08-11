68434e884160b0854cbfc4245e57c4b44ab18216
Diksha Jaiswal <dikshajaiswal111@gmail.com>
2024-08-11 13:59:18 -0400

    React: Tic-Tac-Toe: Reset Game on clicking Rematch button
    Lecture 93
    
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
   let activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(array => [...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -53,6 +53,10 @@ function App() {
     });
   }
 
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -60,7 +64,7 @@ function App() {
           <Player initialName="Player 1" symbol='X' isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol='O' isActive={activePlayer === 'O'} />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard onSelectSquare={handleSquareSelect} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
